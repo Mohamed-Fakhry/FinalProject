4934b5bc79978b3f7f53b2a8899ee7d65bd08fc0
Mohamed Fakhry <mohamedfakhry58890@gmail.com>
2017-12-29 23:19:37 +0200

    Step 5: Add a Paid Flavor

---
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -0,0 +1,27 @@
+package com.udacity.gradle.builditbigger;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Mohamed Fakhry on 29/12/2017.
+ */
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends android.support.v4.app.Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+        return root;
+    }
+}
